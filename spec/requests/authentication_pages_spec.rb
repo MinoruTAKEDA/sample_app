0e1ed1644ffe7ec36c25de26a38d0a44e85bc44a
MinoruTAKEDA <butamin.bar.git@gmail.com>
2013-11-17 23:16:25 +0900

    Add user following 10.3

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -117,6 +117,16 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Following') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Followers') }
+        end
       end
 
       describe "in the Microposts controller" do
@@ -129,6 +139,18 @@ describe "Authentication" do
         describe "submitting to the destroy action" do
           before { delete micropost_path(FactoryGirl.create(:micropost)) }
           specify { response.should redirect_to(signin_path) }
+        end
+      end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }          
         end
       end
     end
@@ -148,6 +170,7 @@ describe "Authentication" do
         specify { response.should redirect_to(root_path) }
       end
     end
+
     describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:non_admin) { FactoryGirl.create(:user) }
